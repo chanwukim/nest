a72c50b020db48ba9a9bc08d552ffbf92c1b2473
ChanWu Kim <chnwu.kim@gmail.com>
2025-03-12 20:17:39 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -45,6 +45,9 @@ const config = tseslint.config(
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-interface": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/consistent-type-imports": [
         "error",
         {
